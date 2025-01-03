cc1f9e01b771f67b99e8893976797bf7a6572c70
R0XER5 <alitarikaydin20@gmail.com>
2025-01-03 19:06:36 +0300

    Logo Eklendi 03.01.2025

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-
+import logo from "src/assets/images/logo.png"
 import {
-  CCloseButton,
+  CCloseButton, CFormLabel,
   CSidebar,
   CSidebarBrand,
   CSidebarFooter,
@@ -14,6 +14,8 @@ import { AppSidebarNav } from './AppSidebarNav'
 
 // sidebar nav config
 import navigation from '../_nav'
+import CIcon from "@coreui/icons-react";
+import {Link} from "react-router-dom";
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
@@ -32,9 +34,23 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarHeader className="border-bottom">
-        <CSidebarBrand to="/">
-          {/*<CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />*/}
-          {/*<CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />*/}
+        <CSidebarBrand className="d-none d-md-flex">
+          <CFormLabel
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "white",
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              textDecoration: "none",
+
+            }}
+          >
+            <img src={logo} alt="Logo" height={40} />
+            <span style={{ marginLeft: "8px",textDecoration: "none"
+            }}>Zino Kriptoloji</span>
+          </CFormLabel>
+
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
